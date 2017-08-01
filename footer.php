560c7aeaf1fced5b7748085d58e3222146c6244e
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-08-01 20:17:06 +0200

    Add custom footer sidebar

--- footer.php
+++ footer.php
@@ -4,14 +4,38 @@
  *
  * Contains the closing of the #content div and all content after.
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * @link    https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package WPBootstrap
  */
 ?>
 
-    </div> <!-- /#content -->
+<footer id="footer-wrap">
+    <div class="container">
+        <div class="row">
+            <?php if ( is_active_sidebar( 'wp_bootstrap-footer-left-sidebar' ) ): ?>
+                <div class="col-lg-4">
+                    <?php dynamic_sidebar( 'wp_bootstrap-footer-left-sidebar' ); ?>
+                </div>
+            <?php endif; ?>
 
-    <?php wp_footer(); ?>
-    </body>
+            <?php if ( is_active_sidebar( 'wp_bootstrap-footer-middle-sidebar' ) ): ?>
+                <div class="col-lg-4">
+                    <?php dynamic_sidebar( 'wp_bootstrap-footer-middle-sidebar' ); ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if ( is_active_sidebar( 'wp_bootstrap-footer-right-sidebar' ) ): ?>
+                <div class="col-lg-4">
+                    <?php dynamic_sidebar( 'wp_bootstrap-footer-right-sidebar' ); ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</footer>
+
+</div> <!-- /#content -->
+
+<?php wp_footer(); ?>
+</body>
 </html>
