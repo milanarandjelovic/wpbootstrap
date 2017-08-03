b628ea3877377edb3cba8c43838c322503a59c31
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-08-03 12:46:42 +0200

    Add custom function to print comments

---
+++ inc/templates/comments/wp-bootstrap-comments-nav.php
@@ -0,0 +1,13 @@
+<nav class="comment__navigation">
+    <div class="comment__nav-links">
+
+        <div class="comment__nav-previous">
+            <?php previous_comments_link( esc_html__( 'Older Comments', 'wp_bootstrap' ) ); ?>
+        </div> <!-- /.comment__nav-previous -->
+
+        <div class="comment__nav-next">
+            <?php next_comments_link( esc_html__( 'Newer Comments', 'wp_bootstrap' ) ); ?>
+        </div> <!-- /.comment__nav-next -->
+
+    </div> <!-- /.comment__nav-links -->
+</nav> <!-- /.comment__navigation -->
