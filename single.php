5a75ab4ff5df4ca49c37275d417e406aebe1e84e
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-08-03 18:58:53 +0200

    Use template part for navigation

--- single.php
+++ single.php
@@ -10,32 +10,7 @@
 get_header();
 ?>
 
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a href="<?php echo home_url(); ?>" class="navbar-brand"><?php bloginfo( 'name' ); ?></a>
-        </div> <!-- /.navbar-header -->
-        <div class="navbar-collapse collapse navbar-right">
-            <?php wp_nav_menu( array(
-                'menu'            => 'primary',
-                'theme_location'  => 'primary',
-                'depth'           => 2,
-                'container'       => 'div',
-                'container_class' => 'navbar-collapse collapse navbar-right',
-                'container_id'    => 'primary',
-                'menu_class'      => 'nav navbar-nav',
-                'fallback_cb'     => 'wp_bootstrap_navbalker::fallback',
-                'walker'          => new wp_bootstrap_navwalker(),
-            ) ); ?>
-        </div> <!-- /.navbar-collapse -->
-    </div> <!-- /.container -->
-</nav> <!-- /.navbar -->
+<?php get_template_part( 'template-parts/header/navigation' ); ?>
 
 <?php if ( ! is_front_page() ): ?>
     <div class="header__wrapper">
