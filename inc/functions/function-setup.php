bac1a2726ca41149aaa06481dc2cefdd4cd66b18
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-08-05 14:10:17 +0200

    Enable support for post formats

--- inc/functions/function-setup.php
+++ inc/functions/function-setup.php
@@ -51,6 +51,23 @@ if ( ! function_exists( 'wp_bootstrap_setup' ) ):
             'gallery',
             'caption',
         ) );
+
+        /**
+         * Enable support for Post Formats.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/post-formats/
+         */
+        add_theme_support( 'post-formats', array(
+            'aside',
+            'gallery',
+            'link',
+            'image',
+            'quote',
+            'status',
+            'video',
+            'audio',
+            'chat',
+        ) );
     }
 
     add_action( 'after_setup_theme', 'wp_bootstrap_setup' );
