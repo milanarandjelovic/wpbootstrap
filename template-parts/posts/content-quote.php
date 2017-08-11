c89f416855e2413ef4a507bedd42363f55cf6808
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-08-11 02:17:42 +0200

    Add template for dsiplaying Quote Post format

---
+++ template-parts/posts/content-quote.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template part for displaying quote post format.
+ *
+ * @link    https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WPBootstrap
+ */
+?>
+
+<article id="<?php the_ID(); ?>" <?php post_class( 'post__holder wp_bootstrap_quote_holder' ); ?>>
+
+    <header class="entry-header">
+
+        <?php echo wp_bootstrap_post_meta(); ?>
+
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1 col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
+                <h1 class="quote-content text-center">
+                    <a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
+                    <?php the_title( '<h2 class="quote-author text-center">- ', ' -</h2>' ); ?>
+                </h1> <!-- /.quote-content -->
+            </div>
+        </div>
+
+    </header> <!-- /.entry-header -->
+
+    <footer class="entry-footer">
+        <?php echo wp_bootstrap_post_footer(); ?>
+    </footer> <!-- /.entry-footer -->
+
+</article>
