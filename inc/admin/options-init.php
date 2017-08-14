085110972cdb93357751803cd019fbbfb2a39fd9
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2017-08-14 06:36:43 +0200

    Added some example for redux framework

--- inc/admin/options-init.php
+++ inc/admin/options-init.php
@@ -11,7 +11,7 @@ if ( ! class_exists( 'Redux' ) ) {
 }
 
 // This is your option name where all the Redux data is stored.
-$opt_name = "wp_bootstrap";
+$opt_name = "wp_bootstrap_data";
 
 /**
  * ---> SET ARGUMENTS
@@ -22,7 +22,7 @@ $opt_name = "wp_bootstrap";
 $theme = wp_get_theme(); // For use with some settings. Not necessary.
 
 $args = array(
-    'opt_name'              => 'wp_bootstrap',
+    'opt_name'              => 'wp_bootstrap_data',
     'dev_mode'              => false,
     'display_name'          => $theme->get( 'Name' ),
     'display_version'       => $theme->get( 'Version' ),
@@ -33,7 +33,7 @@ $args = array(
     'admin_bar'             => true,
     'menu_type'             => 'menu',
     'menu_title'            => esc_html__( 'Theme Options', 'wp_bootstrap' ),
-    'admin_bar_icon'        => 'dashicons-admin-generic',
+    'admin_bar_icon'        => 'dashicons-dashboard',
     'allow_sub_menu'        => true,
     'page_parent_post_type' => 'your_post_type',
     'customizer'            => true,
@@ -95,10 +95,56 @@ Redux::setArgs( $opt_name, $args );
  * Navigation Settings
  */
 Redux::setSection( $opt_name, array(
+    'title'  => esc_html__( 'General Settings', 'wp_bootstrap' ),
+    'id'     => 'wp_bootstrap__section-general',
+    'icon'   => 'el el-cogs',
+    'fields' => array(
+        array(
+            'id'       => 'wp_bootstrap__opt-logo-standard',
+            'type'     => 'media',
+            'url'      => true,
+            'title'    => esc_html__( 'Logo Image', 'wp_bootstrap' ),
+            'compiler' => 'true',
+            'desc'     => esc_html__( 'Upload your image or remove image if you want to use text-based logo.', 'wp_bootstrap' ),
+            'default'  => array(
+                'url' => get_template_directory_uri() . '/assets/images/logo.png',
+            ),
+            'width'    => '',
+            'height'   => '',
+        ),
+        array(
+            'id'             => 'wp_bootstrap__opt-logo-width',
+            'type'           => 'dimensions',
+            'units'          => array( 'px' ),
+            'units_extended' => 'false',
+            'title'          => esc_html__( 'Logo width', 'wp_bootstrap' ),
+            'subtitle'       => esc_html__( 'Set your Logo width.', 'wp_bootstrap' ),
+            'desc'           => esc_html__( 'Logo width can be set in px. Height will automatically calculated.', 'wp_bootstrap' ),
+            'height'         => false,
+            'default'        => array(
+                'width' => 140,
+            ),
+        ),
+    ),
+) );
+
+/**
+ * Navigation Settings
+ */
+Redux::setSection( $opt_name, array(
     'title'  => esc_html__( 'Navigation Settings', 'wp_bootstrap' ),
     'id'     => 'wp_bootstrap__section-navigation',
-    'icon'   => 'el el-arrow-up',
+    'icon'   => 'el el-tasks',
     'fields' => array(
+        array(
+            'id'       => 'wp_bootstrap__navigation-styling',
+            'type'     => 'switch',
+            'title'    => esc_html__( 'Customize Navigation Bar?', 'wp_bootstrap' ),
+            'subtitle' => esc_html__( 'Turn on to change for the Navigation Bar.', 'wp_bootstrap' ),
+            'default'  => 'off',
+            'on'       => esc_html__( 'Yes', 'wp_bootstrap' ),
+            'off'      => esc_html__( 'No', 'wp_bootstrap' ),
+        ),
         array(
             'id'                    => 'wp_bootstrap__navigation-background-color',
             'type'                  => 'background',
@@ -113,6 +159,7 @@ Redux::setSection( $opt_name, array(
             'default'               => array(
                 'background-color' => '#384452',
             ),
+            'required'              => array( 'wp_bootstrap__navigation-styling', '=', '1' ),
         ),
     ),
 ) );
--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -1,5 +1,10 @@
+<?php
+$wp_bootstrap_data = get_option( 'wp_bootstrap_data' );
+$nav_back          = $wp_bootstrap_data['wp_bootstrap__navigation-background-color']['background-color'];
+?>
+
 <!-- NAVIGATION -->
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top" style="background-color: <?php echo $nav_back; ?>">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -8,7 +13,16 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a href="<?php echo home_url(); ?>" class="navbar-brand"><?php bloginfo( 'name' ); ?></a>
+            <?php if ( $wp_bootstrap_data['wp_bootstrap__opt-logo-standard']['url'] !== '' ): ?>
+                <a href="<?php echo home_url(); ?>" class="navbar-brand">
+                    <img src="<?php echo esc_url( $wp_bootstrap_data['wp_bootstrap__opt-logo-standard']['url'] ) ?>"
+                         alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"
+                         title="<?php echo esc_attr( get_bloginfo( 'description' ) ); ?>"
+                    >
+                </a>
+            <?php else: ?>
+                <a href="<?php echo home_url(); ?>" class="navbar-brand"><?php bloginfo( 'name' ); ?></a>
+            <?php endif; ?>
         </div> <!-- /.navbar-header -->
         <div class="navbar-collapse collapse navbar-right">
             <?php wp_nav_menu( array(
